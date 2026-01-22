5b5b33688f54f5236cc4fc72f71df21f74afc600
Thomas Bendall <14180399+tommbendall@users.noreply.github.com>
2026-01-22 13:31:41 +0000

    first attempt at upgrade macro

--- components/driver/rose-meta/lfric-driver/versions.py
+++ components/driver/rose-meta/lfric-driver/versions.py
@@ -31,3 +31,19 @@ class vnXX_txxx(MacroUpgrade):
         # Add settings
         return config, self.reports
 """
+
+class vn30_t238(MacroUpgrade):
+    """Upgrade macro for PR #238 by Thomas Bendall."""
+
+    BEFORE_TAG = "vn3.0"
+    AFTER_TAG = "vn3.0_t238"
+
+    def upgrade(self, config, meta_config=None):
+        # Commands From: rose-meta/lfric-driver
+        self.add_setting(
+            config, ["namelist:finite_element", "coord_space"], "Wchi"
+        )
+        self.add_setting(
+            config, ["namelist:finite_element", "coord_order_multigrid"], "1"
+        )
+        return config, self.reports
