0b705e00b248b1f3dd0debea06021377138a01d5
ukmo-juan.castillo <juan.m.castillo@metoffice.gov.uk>
2026-03-09 15:07:12 +0000

    Add update macros

--- components/driver/rose-meta/lfric-driver/versions.py
+++ components/driver/rose-meta/lfric-driver/versions.py
@@ -31,3 +31,13 @@ class vnXX_txxx(MacroUpgrade):
         # Add settings
         return config, self.reports
 """
+
+class vn31_t301(MacroUpgrade):
+    """Upgrade macro for ticket 301 by Juan M Castillo."""
+
+    BEFORE_TAG = "vn3.1"
+    AFTER_TAG = "vn3.1_t301"
+
+    def upgrade(self, config, meta_config=None):
+        # Blank Upgrade Macro
+        return config, self.reports
