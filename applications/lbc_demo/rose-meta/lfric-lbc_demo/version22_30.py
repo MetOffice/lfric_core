781111bceafe7d62ca23aa6556ea98014a916d37
Ricky Wong <141156427+mo-rickywong@users.noreply.github.com>
2026-04-29 14:58:08 +0100

    Fix existing issues on trunk cause update macro failures

--- applications/lbc_demo/rose-meta/lfric-lbc_demo/version22_30.py
+++ applications/lbc_demo/rose-meta/lfric-lbc_demo/version22_30.py
@@ -39,9 +39,8 @@ class vn22_t4231(MacroUpgrade):
     def upgrade(self, config, meta_config=None):
         # Add settings
         return config, self.reports
+
 """
-
-
 class vn22_t34(MacroUpgrade):
     # Upgrade macro for 34 by jennifer hickson
 
@@ -50,3 +49,4 @@ class vn22_t34(MacroUpgrade):
 
     def upgrade(self, config, meta_config=None):
         return config, self.reports
+"""
