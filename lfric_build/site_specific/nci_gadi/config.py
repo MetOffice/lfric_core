6682d8754580992c54a5f90456d2d9e55922fc16
Joerg Henrichs <henrichsjoerg@gmail.com>
2026-05-06 10:36:24 +1000

    #292 Updated nci gadi config.

--- lfric_build/site_specific/nci_gadi/config.py
+++ lfric_build/site_specific/nci_gadi/config.py
@@ -8,42 +8,89 @@
 
 '''
 This module contains the default configuration for NCI. It will be invoked
-by the Baf scripts. This script:
-- sets intel-classic as the default compiler suite to use.
-- Adds the tau compiler wrapper as (optional) compilers to the ToolRepository.
+by the Fab scripts. This script sets intel-llvm as the default compiler
+suite to use, and adds the required site-specific linker and include flag.
 '''
 
 from fab.api import Category, ToolRepository
+from fab.tools.pfunit import PfUnit
 
 from default.config import Config as DefaultConfig
 
 
 class Config(DefaultConfig):
     '''
-    For NCI, make intel the default, and add the Tau wrapper.
+    For NCI, make intel the default, and setup link paths for gnu,
+    intel-classic and intel-llvm.
     '''
 
     def __init__(self):
         super().__init__()
         tr = ToolRepository()
-        tr.set_default_compiler_suite("intel-classic")
+        tr.set_default_compiler_suite("intel-llvm")
 
-        # ATM we don't use a shell when running a tool, and as such
-        # we can't directly use "$()" as parameter. So query these values using
-        # Fab's shell tool (doesn't really matter which shell we get, so just
-        # ask for the default):
+    def setup_gnu(self, build_config: BuildConfig) -> None:
+        '''
+        This method sets up the Gnu compiler and linker flags.
+        For now call an external function, since it is expected that
+        this configuration can be very lengthy (once we support
+        compiler modes).
+
+        :param build_config: the Fab build configuration instance
+        '''
+        super().setup_gnu(build_config)
+        linker = tr.get_tool(Category.LINKER, "linker-gnu")
+        # Add netcdf and pfunit flags
+        self._setup_linker(linker)
+
+    def setup_intel_classic(self, build_config: BuildConfig) -> None:
+        '''
+        This method sets up the Gnu compiler and linker flags.
+        For now call an external function, since it is expected that
+        this configuration can be very lengthy (once we support
+        compiler modes).
+
+        :param build_config: the Fab build configuration instance
+        '''
+        super().setup_intel_classic(build_config)
+        linker = tr.get_tool(Category.LINKER, "linker-intel-classic")
+        # Add netcdf and pfunit flags
+        self._setup_linker(linker)
+
+    def setup_intel_llvm(self, build_config: BuildConfig) -> None:
+        '''
+        This method sets up the Gnu compiler and linker flags.
+        For now call an external function, since it is expected that
+        this configuration can be very lengthy (once we support
+        compiler modes).
+
+        :param build_config: the Fab build configuration instance
+        '''
+        super().setup_intel_llvm(build_config)
+        linker = tr.get_tool(Category.LINKER, "linker-intel-llvm")
+        # Add netcdf and pfunit flags
+        self._setup_linker(linker)
+
+    def _setup_linker(self, linker: Linker) -> None:
+        """
+        Generic setup of a linker. This adds netcdf and pfunit definitions.
+
+        :param linker: the linker instance to setup
+        """
+        tr = ToolRepository()
         shell = tr.get_default(Category.SHELL)
         # We must remove the trailing new line, and create a list:
         nc_flibs = shell.run(additional_parameters=["-c", "nf-config --flibs"],
                              capture_output=True).strip().split()
-        linker = tr.get_tool(Category.LINKER, "linker-tau-ifort")
+        linker.add_lib_flags("netcdf", nc_flibs, silent_replace=True)
 
-        # Setup all linker flags:
-        linker.add_lib_flags("netcdf", nc_flibs)
-        linker.add_lib_flags("yaxt", ["-lyaxt", "-lyaxt_c"])
-        linker.add_lib_flags("xios", ["-lxios"])
-        linker.add_lib_flags("hdf5", ["-lhdf5"])
-        linker.add_lib_flags("shumlib", ["-lshum"])
+        pfunit = tr.get_tool(Category.PFUNIT, "funitproc")
+        pfunit_root = pfunit.get_root_path()
+        spack_view = os.environ.get("SPACK_ENV_VIEW", "")
 
-        # Always link with C++ libs
-        linker.add_post_lib_flags(["-lstdc++"])
+        linker.add_lib_flags(
+            "pfunit",
+            [f"-L{pfunit_root}/lib", "-lfunit", "-lpfunit",
+             f"-L{spack_view}/FARGPARSE-1.7/lib/", "-lfargparse",
+             f"-L{spack_view}/GFTL_SHARED-1.8/lib", "-lgftl-shared-v2",
+            ], silent_replace=True)
