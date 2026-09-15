a5a56ba9dc99c39eef514556e6c946e0a4e6c31a
Andrew Coughtrie <24609575+andrewcoughtrie@users.noreply.github.com>
2026-09-15 11:56:14 +0100

    Initial refactor of dependerator

--- infrastructure/build/analyse.mk
+++ infrastructure/build/analyse.mk
@@ -12,6 +12,13 @@
 # needs to operate in a single thread regime. We don't want to impose that
 # restriction on the rest of the build system.
 #
+# All out of date source files are handed to a single invocation of the
+# analyser rather than one invocation per file. Starting a Python interpreter,
+# importing its modules and opening the database for every source file
+# dominated the cost of this phase. The analyser parallelises the reading and
+# preprocessing of source internally while keeping database access on a single
+# thread.
+#
 # The following variables may be specified to modify behaviour:
 #
 # PRE_PROCESS_INCLUDE_DIRS: Space separated list of directories to search for
@@ -26,7 +33,12 @@
 DATABASE ?= dependencies.db
 
 SOURCE_FILES := $(subst ./,,$(shell find . -name '*.[Ff]90' -print))
-TOUCH_FILES = $(subst .F90,.t,$(subst .f90,.t,$(SOURCE_FILES)))
+
+# Record of which sources have been analysed. Make compares the modification
+# time of this file against each source so only those which have changed are
+# presented to the analyser, through $?.
+#
+ANALYSED_STAMP = analysed.stamp
 
 programs.mk: dependencies.mk
 	$(call MESSAGE,Collating,$@)
@@ -34,7 +46,7 @@ programs.mk: dependencies.mk
                                                 -database $(DATABASE) \
 	                                        -objectdir . $@
 
-dependencies.mk: $(TOUCH_FILES)
+dependencies.mk: $(ANALYSED_STAMP)
 	$(call MESSAGE,Building,$@)
 	$(Q)$(LFRIC_BUILD)/tools/DependencyRules $(VERBOSE_ARG) \
                                                  -database $(DATABASE) \
@@ -46,17 +58,18 @@ IGNORE_ARGUMENTS = $(addprefix -ignore ,$(IGNORE_DEPENDENCIES))
 INCLUDE_ARGUMENTS = $(addprefix -include , $(PRE_PROCESS_INCLUDE_DIRS))
 MACRO_ARGUMENTS = $(addprefix -macro , $(PRE_PROCESS_MACROS))
 
-%.t: %.f90
-	$(call MESSAGE,Analysing,$<)
-	$(Q)$(LFRIC_BUILD)/tools/DependencyAnalyser \
-	    $(IGNORE_ARGUMENTS) $(VERBOSE_ARG) $(DATABASE) $<
-	$(Q)touch $@
-
-%.t: %.F90
-	$(call MESSAGE,Analysing,$<)
+# All out of date sources are passed to a single invocation of the analyser.
+# Launching one Python interpreter per source file dominated the cost of this
+# phase of the build.
+#
+# The analyser removes a file's existing entries before rescanning it so
+# presenting only the changed subset leaves the database consistent.
+#
+$(ANALYSED_STAMP): $(SOURCE_FILES)
+	$(call MESSAGE,Analysing,$(words $?) source files)
 	$(Q)$(LFRIC_BUILD)/tools/DependencyAnalyser \
 	    $(IGNORE_ARGUMENTS) $(INCLUDE_ARGUMENTS) $(MACRO_ARGUMENTS) \
-	    $(VERBOSE_ARG) $(DATABASE) $<
+	    $(VERBOSE_ARG) $(DATABASE) $?
 	$(Q)touch $@
 
 include $(LFRIC_BUILD)/lfric.mk
